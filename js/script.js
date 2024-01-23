d79ab2335f985dc90227326b6a2cf59f0115e166
RiccardoGarbo <Riccardo.garbo1994@gmail.com>
2024-01-23 18:56:47 +0100

    add button for add new itemList

--- js/script.js
+++ js/script.js
@@ -2,15 +2,21 @@ const { createApp } = Vue
 const app = createApp({
     data() {
         return {
-            listItems
+            listItems,
+            newListItem: { id: 4, done: false, text: '' }
 
         }
     },
     methods: {
+        //funzione per cantellare un item
         deleteItem(id) {
             this.listItems = this.listItems.filter(listItem => id !== listItem.id)
-
+        },
+        //funzione per aggiungere un item
+        addListItem() {
+            this.listItems.push(this.newListItem)
         }
-    }
+    },
+
 })
 app.mount('#root')
